62b1c1460478acb5ce1ab2b0aacb5aba1d3b466d
RismyCJ <162423029+RismyCJ@users.noreply.github.com>
2024-03-12 16:15:21 +0530

    Update calculator.java
    changes added

--- src/main/java/calculator/calculator.java
+++ src/main/java/calculator/calculator.java
@@ -29,7 +29,7 @@ class calculator extends JFrame implements ActionListener {
 		f = new JFrame("calculator");
 
 		try {
-			System.out.println("TEST...................");
+			System.out.println("TEST  calculator...................");
 			// set look and feel
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		}
